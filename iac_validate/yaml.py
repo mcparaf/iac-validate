bd7f27952e04ece29de4299b2da0372f4414542f
Marcin Parafiniuk <maparafi@cisco.com>
2024-08-02 10:27:52 +0200

    replace custom merge logic with mergedeep.merge

--- iac_validate/yaml.py
+++ iac_validate/yaml.py
@@ -7,6 +7,7 @@ import logging
 import os
 import subprocess
 from typing import Any, Dict, List
+from mergedeep import merge, Strategy
 
 from ruamel import yaml
 
@@ -72,7 +73,7 @@ def load_yaml_files(paths: List[str]) -> Dict[str, Any]:
                 y.register_class(VaultTag)
                 y.register_class(EnvTag)
                 dict = y.load(data_yaml)
-                merge_dict(dict, data)
+                merge(data, dict, strategy=Strategy.TYPESAFE_ADDITIVE) 
 
     result: Dict[str, Any] = {}
     for path in paths:
@@ -86,69 +87,3 @@ def load_yaml_files(paths: List[str]) -> Dict[str, Any]:
                     except:  # noqa: E722
                         logger.warning("Could not load file: {}".format(filename))
     return result
-
-
-def merge_list_item(
-    source_item: Any, destination: List[Any], merge_list_items: bool = True
-) -> None:
-    """Merge item into list."""
-    if isinstance(source_item, dict) and merge_list_items:
-        # check if we have an item in destination with matching primitives
-        for dest_item in destination:
-            match = True
-            comparison = False
-            unique_source = False
-            unique_dest = False
-            for k, v in source_item.items():
-                if isinstance(v, dict) or isinstance(v, list):
-                    continue
-                if k in dest_item and v == dest_item[k]:
-                    comparison = True
-                    continue
-                if k not in dest_item:
-                    unique_source = True
-                    continue
-                comparison = True
-                match = False
-            for k, v in dest_item.items():
-                if isinstance(v, dict) or isinstance(v, list):
-                    continue
-                if k in source_item and v == source_item[k]:
-                    comparison = True
-                    continue
-                if k not in source_item:
-                    unique_dest = True
-                    continue
-                comparison = True
-                match = False
-            if comparison and match and not (unique_source and unique_dest):
-                merge_dict(source_item, dest_item, merge_list_items)
-                return
-    elif source_item in destination:
-        return
-    destination.append(source_item)
-
-
-def merge_dict(
-    source: Dict[Any, Any], destination: Dict[Any, Any], merge_list_items: bool = True
-) -> Dict[Any, Any]:
-    """Merge two nested dict/list structures."""
-    if not source:
-        return destination
-    for key, value in source.items():
-        if isinstance(value, dict):
-            # get node or create one
-            node = destination.setdefault(key, {})
-            if node is None:
-                destination[key] = value
-            else:
-                merge_dict(value, node, merge_list_items)
-        elif isinstance(value, list):
-            if key not in destination:
-                destination[key] = value
-            if isinstance(destination[key], list):
-                for i in value:
-                    merge_list_item(i, destination[key], merge_list_items)
-        else:
-            destination[key] = value
-    return destination
